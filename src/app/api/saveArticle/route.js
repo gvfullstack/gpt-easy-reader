f80d8dba654fec084d3196af5b4d5dcf622a61de
gvfullstack <26497670+gvfullstack@users.noreply.github.com>
2024-11-16 11:15:38 -0800

    fb admin convert2

--- src/app/api/saveArticle/route.js
+++ src/app/api/saveArticle/route.js
@@ -1,4 +1,4 @@
-import { firestore } from "../../../../firebaseAdminConfig"; // Adjust path if necessary
+import { firestore } from "../../../../firebaseAdminConfig"; 
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
